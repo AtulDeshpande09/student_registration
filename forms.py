520358ae2207b33f91a12bd85ee200a36dc340b3
Atul Deshpande <159980686+AtulDeshpande09@users.noreply.github.com>
2024-11-11 20:52:42 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,21 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField , SubmitField
-from wtforms.validators import DataRequired , Email , Length
+from wtforms import StringField, PasswordField, SubmitField, RadioField
+from wtforms.validators import DataRequired, Email, Length
 
-class RegistrationForm:
-
-    name = StringField('Name',validators=[])
-    email = StringField('Email',validators=[])
-
-    password = PasswordField('Password', validators=[])
-
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    gender = RadioField('Gender', choices=[('M', 'Male'), ('F', 'Female')], validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Register')
 
-
-class LoginForm:
-
-    email = StringField('Email',validators=[])
-
-    password = PasswordField('Password', validators=[])
-
-    submit = SubmitField('Register')
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
